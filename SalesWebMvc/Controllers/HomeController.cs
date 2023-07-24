86506f284a408dcbc8e6cd27ee909653cfef00f3
Lucas Silveira <lukas.silveira@hotmail.com>
2023-07-24 15:08:02 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["email"] = "lucasssilveira7@outlook.com";
 
             return View();
         }
